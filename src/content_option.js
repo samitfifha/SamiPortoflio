4a17bb60d44c16b1e88dc560054d00755180f9cd
samitfifha <47883414+samitfifha@users.noreply.github.com>
2022-08-10 17:19:07 +0100

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,307 @@
+const logotext = "Sami";
+const meta = {
+    title: "Sami Tfifha",
+    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Tfifha Sami",
+    animated: {
+        first: "Engineering Student",
+        second: "Designer / Developer",
+        third: "Event & Artist Manager / Photographer / Videographer",
+    },
+    description: "Web/Mobile developer for 2 years, designer for 5 years, expert in graphic design, amateur photo/videographer as well as Video editor for 2 years and artist and event manager since 2020",
+    your_img_url: "https://cdn.discordapp.com/attachments/782697628529197147/1006705128742592562/113-01.jpg",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Student in 2nd year of engineering computer and mobile systems at the Superior Private School of Engineering and Technologies, Web/ Mobile developer for 2 years, designer and editor for 4 years and amateur photographer/ videographer for 1 years and event promoter since 2013 and artist manager since 2020 and not that.. having also experience in artistic direction, I am always looking for new experiences, new opportunities and new challenges to meet, Whether in the field of marketing, event, artistic or Mobile/ Web development, I am always at your disposal to give you the services you want",
+};
+const worktimeline = [{
+        jobtitle: "Mobile developer & UI designer intern",
+        where: "MS Solutions Group",
+        date: "July - September 2022",
+    },
+    {
+        jobtitle: "Web developer intern",
+        where: "ABS Network",
+        date: "August - September 2019",
+    },
+    {
+        jobtitle: "Designer",
+        where: "Freelancer",
+        date: "2017 - Present",
+    },
+    {
+        jobtitle: "Artist manager",
+        where: "Dixmount - DJ/ Producer (Tunisia)",
+        date: "2020 - Present",
+    },
+    {
+        jobtitle: "Video editor",
+        where: "Freelancer",
+        date: "2019 - Present",
+    },
+    {
+        jobtitle: "Photographer / Videographer ",
+        where: "Freelancer",
+        date: "2019 - Present",
+    },
+    {
+        jobtitle: "Event manager",
+        where: "Freelancer",
+        date: "2015 - Present",
+    },
+    {
+        jobtitle: "Event manager",
+        where: "SpreeEvents (Whatever Saloon, Tunisia)",
+        date: "2013 - 2015",
+    },
+
+];
+
+const studiestimeline = [
+    {
+        jobtitle: "Diploma in Software Engineering ",
+        where: "ESPRIT",
+        date: "June 2023",
+    },
+{
+    jobtitle: "Certificate in digital design",
+    where: "3S Formation",
+    date: "May 2018",
+},
+
+{
+    jobtitle: "diploma in Russian language",
+    where: "Ryazan State Radio Engineering University",
+    date: "May 2017",
+},
+{
+    jobtitle: "baccalaureate degree in technical science",
+    where: "Ibn Rashiq high school",
+    date: "June 2014",
+},
+
+
+
+];
+
+const Devskills = [
+    {
+        name: "Flutter",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 35,
+    },
+    {
+        name: "C#",
+        value: 30,
+    },
+    {
+        name: "Swift UI / IOS Development",
+        value: 75,
+    },
+    {
+        name: "Kotlin / Android Development",
+        value: 70,
+    },
+    {
+        name: "Native C / C++",
+        value: 45,
+    },
+    {
+        name: "Java /JAVA FX",
+        value: 55,
+    },
+    {
+        name: "Codename One",
+        value: 45,
+    },
+    {
+        name: "Database Management Systems",
+        value: 65,
+    },
+];
+
+
+
+const Desskills = [
+    {
+        name: "Adobe Photoshop",
+        value: 85,
+    },
+    {
+        name: "Adobe Illustrator",
+        value: 95,
+    },
+    {
+        name: "Adobe XD",
+        value: 75,
+    },
+    {
+        name: "Adobe After Effects",
+        value: 45,
+    },
+    {
+        name: "Adobe Premiere Pro",
+        value: 55,
+    },
+    {
+        name: "Adobe indesign",
+        value: 70,
+    },
+    {
+        name: "Figma",
+        value: 85,
+    },
+    
+];
+const Lanskills = [
+    {
+        name: "Arabic",
+        value: 95,
+    },
+    {
+        name: "French",
+        value: 90,
+    },
+    {
+        name: "Russian",
+        value: 75,
+    },
+    {
+        name: "English",
+        value: 65,
+    },
+
+    {
+        name: "Italian",
+        value: 35,
+    },
+    
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://cdn.discordapp.com/attachments/782697628529197147/1006874525566247002/mockup1.png",
+        desctiption: "Banking app Ui Design & Development done in my last internship for MS Solutions",
+        link: "#",
+    },
+    {
+        img: "https://cdn.discordapp.com/attachments/782697628529197147/1006878624886034542/mockup3.png",
+        desctiption: "Banking app Ui Design & Development done in my last internship for MS Solutions",
+        link: "#",
+    },
+    {
+        img: "https://cdn.discordapp.com/attachments/782697628529197147/1006878625448083507/mockup2.png",
+        desctiption: "Fashion app Ui Design & Development done as an academic project in ESPRIT ",
+        link: "#",
+    },
+
+    {
+        img: "https://cdn.discordapp.com/attachments/782697628529197147/1006881789563900025/ezgif.com-gif-maker.gif",
+        desctiption: "Banking app Ui Design & Development done in my last internship for MS Solutions",
+        link: "#",
+    },
+
+    {
+        img: "https://cdn.discordapp.com/attachments/782697628529197147/1006885981758165095/39.png",
+        desctiption: "Job app Ui Design & Development done as a personal project developed in Flutter",
+        link: "#",
+    },
+    {
+        img: "https://cdn.discordapp.com/attachments/782697628529197147/1006888924834177144/ezgif.com-gif-maker_2.gif",
+        desctiption: "Learning app Ui Design & Development done as an academic project devoloped in flutter developed in Flutter",
+        link: "#",
+    },
+];
+
+const dataportfolio1 = [{
+    img: "https://cdn.discordapp.com/attachments/782697628529197147/1006863453060866139/CHRIS_2.jpg",
+    desctiption: "001",
+    link: "#",
+},
+{
+    img: "https://cdn.discordapp.com/attachments/782697628529197147/1006852269175746620/ACID_LANE.jpg",
+    desctiption: "002",
+    link: "#",
+},
+{
+    img: "https://cdn.discordapp.com/attachments/782697628529197147/1006858728663945238/bhar_acid_noir.jpg",
+    desctiption: "003",
+    link: "#",
+},
+{
+    img: "https://cdn.discordapp.com/attachments/782697628529197147/1006858729066610840/chris_1.jpg",
+    desctiption: "004",
+    link: "#",
+},
+{
+    img: "https://cdn.discordapp.com/attachments/782697628529197147/1006858729397944330/MANAR.jpg",
+    desctiption: "005",
+    link: "#",
+},
+{
+    img: "https://cdn.discordapp.com/attachments/782697628529197147/1006858730194870292/veracity.jpg",
+    desctiption: "006",
+    link: "#",
+},
+
+
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "Sami.tfifha@gmail.com",
+    YOUR_EMAIL1: "Sami.tfifha@esprit.tn ",
+    YOUR_FONE: "+216 53 572 801",
+    YOUR_ADD: "35 Boulvard of alger, Ben Arous, Tunisia",
+    description: "Thank you in advance for your attention to my portfolio. \n I am at your disposal for any information you deem useful.. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/samitfifha",
+    linkedin: "https://www.linkedin.com/in/samitfifha/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    dataportfolio1,
+    worktimeline,
+    studiestimeline,
+    Devskills,
+    Desskills,
+    Lanskills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
---
+++ src/pages/portfolio/index.js
@@ -0,0 +1,58 @@
+import React from "react";
+import "./style.css";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Container, Row, Col } from "react-bootstrap";
+import { dataportfolio,dataportfolio1, meta } from "../../content_option";
+
+
+export const Portfolio = () => {
+  return (
+    <HelmetProvider>
+      <Container className="About-header">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title> Portfolio | {meta.title} </title>{" "}
+          <meta name="description" content={meta.description} />
+        </Helmet>
+        <Row className="mb-5 mt-3">
+          <Col lg="8">
+            <h1 className="display-4 mb-4"> UI / UX Design & Development </h1>{" "}
+            <hr className="t_border my-4 ml-0 text-left" />
+          </Col>
+        </Row>
+        <div className="mb-5 po_items_ho">
+          {dataportfolio.map((data, i) => {
+            return (
+              <div key={i} className="po_item">
+                <img src={data.img} alt="" />
+                <div className="content">
+                  <p>{data.desctiption}</p>
+                  
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <Row className="mb-5 mt-3">
+          <Col lg="8">
+            <h1 className="display-4 mb-4"> Poster Design </h1>{" "}
+            <hr className="t_border my-4 ml-0 text-left" />
+          </Col>
+        </Row>
+        <div className="mb-5 po_items_ho">
+          {dataportfolio1.map((data, i) => {
+            return (
+              <div key={i} className="po_item">
+                <img src={data.img} alt="" />
+                <div className="content">
+                  <p>{data.desctiption}</p>
+                  
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </Container>
+    </HelmetProvider>
+  );
+};
